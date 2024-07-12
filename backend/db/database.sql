7d719f7182d12a80edff9e5572a2180cfe977cec
Mehemud Azad <mr.x@MehemudAzad.local>
2024-07-12 16:04:40 +0600

    backend startup2

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -5,5 +5,34 @@ CREATE TABLE users (
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     role TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE
+    email TEXT NOT NULL UNIQUE,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE clients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    firstname TEXT ,
+    lastname TEXT,
+    dateOfBirth TEXT, -- You can use TEXT to store dates or use a specific format 
+    contactNo TEXT,
+    profileImg TEXT,
+    gender TEXT
+);
+
+
+CREATE TABLE therapists (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    firstname TEXT,
+    lastname TEXT,
+    dateOfBirth TEXT, -- You can use TEXT to store dates or use a specific format
+    experience TEXT, -- Assuming this is text-based, could be INTEGER for years of experience
+    contactNo TEXT,
+    profileImg TEXT,
+    gender TEXT
+);
+
