7dbc4dd2b292027d6b1995c916bcd28c9d0611ca
K. M. Mehemud Azad <108463132+MehemudAzad@users.noreply.github.com>
2024-07-12 19:40:55 +0600

    Add files via upload

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dbPromise from '../db/db_init.js';
+import catchAsync from '../utils/catchAsync.js';
+import sendResponse from '../utils/sendResponse.js';
+
+
+const get_users = catchAsync(async (req, res) => {
+    const db = await dbPromise;
+    const result = await db.all("SELECT * FROM users");
+
+    sendResponse(res,{
+        statusCode: 200,
+        success: true,
+        message: "users retrieved successfully",
+        data: result,
+    });
+})
+
+
+export const UserController = {
+    get_users,
+    
+}
