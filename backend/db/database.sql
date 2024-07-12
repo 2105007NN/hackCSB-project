60965e47e2dd9385eb21c94265f1dc1af7315926
sleepytmzd <depressionstonks420@gmail.com>
2024-07-12 19:46:22 +0600

    add tables

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -36,3 +36,25 @@ CREATE TABLE therapists (
     gender TEXT
 );
 
+DROP TABLE IF EXISTS room;
+CREATE TABLE IF NOT EXISTS room (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE  NOT NULL,
+    user1 TEXT NOT NULL,
+    user2 TEXT NOT NULL,
+
+    FOREIGN KEY (user1) REFERENCES users(username),
+    FOREIGN KEY (user1) REFERENCES users(username)
+);
+
+DROP TABLE IF EXISTS message;
+    CREATE TABLE IF NOT EXISTS message (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT NOT NULL,
+    author TEXT NOT NULL,
+    time TEXT NOT NULL,
+    room_id INTEGER,
+    FOREIGN KEY (author) REFERENCES users(username),
+    FOREIGN KEY (room_id) REFERENCES room(id)
+);
+
