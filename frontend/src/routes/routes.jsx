6d55529bfac4f573b16eac56faba36247077b25e
Nafis Nahian <2105007@ugrad.cse.buet.ac.bd>
2024-07-13 22:28:40 +0600

    import error fix in routes

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -13,7 +13,7 @@ import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Support from "../pages/Support/Support";
 import ClientDashboard from "../pages/dashboard/ClientDashboard";
 import ClientProfile from "../pages/Profile/ClientProfile";
-import CreateTest from "../pages/Tests/createTest";
+import CreateTest from "../pages/Tests/CreateTest";
 
 export const routes = createBrowserRouter([
   {
