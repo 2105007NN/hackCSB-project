6a0802f554fb82681d4fe44fd3302e7817898298
Nafis Nahian <2105007@ugrad.cse.buet.ac.bd>
2024-07-15 00:58:49 +0600

    completed backend for journals and mood ratings

--- backend/controllers/Tools.js
+++ backend/controllers/Tools.js
@@ -1,24 +1,68 @@
 import dbPromise from "../db/db_init.js";
-import {ApiError} from '../utils/ApiError.js'
-import {ApiResponse} from '../utils/ApiResponse.js'
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { verifyToken } from "../utils/handleJWT.js";
 
-const journalController = async(req, res) => {
-    console.log(req.body);
-    res.status(200).json({
-        msg : "success",
-        receivedData : req.body
-    })
+const journalController = async (req, res) => {
+	try {
+		const db = await dbPromise;
+		const journalContent = req.body.content;
+		const decodedToken = verifyToken(
+			req.body.access_token,
+			process.env.JWT_ACCESS_SECRET
+		);
+		const userId = decodedToken.userId;
 
-}
+        console.log("user Info ", decodedToken);
 
-const moodTrackerController = async(req, res) => {
-    console.log(req.body);
-    res.status(200).json({
-        msg : "success",
-        receivedData : req.body
-        
-    })
-    
-}
+		const insertJournalSQL = `INSERT INTO journals(user_id, content, createdAt) 
+            VALUES (?, ?, datetime('now'))`;
 
-export {journalController, moodTrackerController};
+		await db.run(insertJournalSQL, [userId, journalContent]);
+
+        const dbEntry = await db.all(`SELECT * FROM journals WHERE user_id = ${userId}`);
+        console.log(`for userID : ${userId}, Journal Table : ${dbEntry}`);
+
+		
+		res.status(200).json({
+			msg: "success",
+			dbResponse: dbEntry,
+		});
+	} catch (error) {
+		console.log("ERROR IN Journal CONTROLLER : ", error);
+		res.status(500).json(
+			new ApiError(400, "ERROR IN Journal CONTROLLER")
+		);
+	}
+};
+
+const moodTrackerController = async (req, res) => {
+	try {
+		const db = await dbPromise;
+		const moodRating = req.body.moodRating;
+		const decodedToken = verifyToken(
+			req.body.access_token,
+			process.env.JWT_ACCESS_SECRET
+		);
+		const userId = decodedToken.userId;
+		const insertMoodRatingSQL = `INSERT INTO mood_ratings(user_id, rating, createdAt) 
+            VALUES (?, ?, datetime('now'))`;
+
+		await db.run(insertMoodRatingSQL, [userId, moodRating]);
+
+        const dbEntry = await db.all(`SELECT * FROM mood_ratings WHERE user_id = ?`, [userId]);
+        console.log(`for userID : ${userId}, mood_ratings Table : ${dbEntry}`);
+
+		res.status(200).json({
+			msg: "success",
+			dbResponse: dbEntry,
+		});
+	} catch (error) {
+		console.log("ERROR IN MOOD RATING CONTROLLER : ", error);
+		res.status(500).json(
+			new ApiError(400, "ERROR IN MOOD RATING CONTROLLER")
+		);
+	}
+};
+
+export { journalController, moodTrackerController };
