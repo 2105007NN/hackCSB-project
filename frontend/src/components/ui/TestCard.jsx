acc8bc0dbbcf76df61d0d43b514469963cc48146
K. M. Mehemud Azad <108463132+MehemudAzad@users.noreply.github.com>
2024-08-30 15:17:17 +0600

    Update TestCard.jsx

--- frontend/src/components/ui/TestCard.jsx
+++ frontend/src/components/ui/TestCard.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
-
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider";
 
 // eslint-disable-next-line react/prop-types
 const TestCard = ({ test }) => {
 //   const { title, description } = test;
+    const {user} = useContext(AuthContext);
   return (
     <div className="card bg-base-300 w-96 shadow-xl">
       <figure className="bg-primary h-24">
@@ -17,11 +19,18 @@ const TestCard = ({ test }) => {
          {test?.description}
         </p>
         <div className="card-actions justify-start">
-            <Link to={`/take-test/${test?.id}`}>
-                <button className="btn btn-primary text-xl text-white mt-8">
-                    Start test
-                </button>
-            </Link>
+            {
+            user?.id ? <Link to={`/take-test/${test?.id}`}>
+            <button className="btn btn-primary text-xl text-white mt-8">
+                Start test
+            </button>
+        </Link> :
+          <Link to={`/auth/login`}>
+          <button className="btn btn-primary text-xl text-white mt-8">
+              Start test
+          </button>
+      </Link> 
+          }
         </div>
       </div>
     </div>
